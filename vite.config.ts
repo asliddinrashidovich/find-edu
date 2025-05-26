7854f5a53caa3596487682aae1473ec066161d6a
Asliddin <asliddinrashidovich7@gmail.com>
2025-05-26 19:39:59 +0500

    the bug is fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
+// ESM versiya: __dirname o‘rnini bosish
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
