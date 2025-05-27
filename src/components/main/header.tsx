69cf21a297fa98f1b512c5cfbd9300f51f37ab38
Asliddin <asliddinrashidovich7@gmail.com>
2025-05-27 12:58:00 +0500

    the search is add

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -71,7 +71,7 @@ function Header() {
                   { value: 'ru', label: 'ru' },
               ]}
           />
-          {!token && <div className="flex gap-[7px] items-center">
+          {!token && <div className="hidden md:flex gap-[7px] items-center">
               <Link to={'/login'} className="border-[1px] border-[#461773] text-[#461773] rounded-[30px] cursor-pointer px-[20px] py-[5px]">
                 Kirish
               </Link>
